77d72fe821b6339ad16e9fe90ffc8d775c938464
Ivan Krasin <imkrasin@gmail.com>
2013-02-16 22:15:23 +0000

    Add stub for Weather playground

---
+++ weather/main.go
@@ -0,0 +1,12 @@
+// This code is a playground for exersises 2.8 from the book
+// Probabilistic Robotics by Sebastian Thrun, Wolfram Burgard, Dieter Fox
+// ISBN-13: 978-0-262-20162-9
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	fmt.Printf("Sunny\n")
+}
