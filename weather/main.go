7a5eb035845d0d5b1974fe98471862c0bfbe7256
Ivan Krasin <imkrasin@gmail.com>
2013-02-16 22:54:13 +0000

    Simulate weather transition

--- weather/main.go
+++ weather/main.go
@@ -5,8 +5,44 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
+type MarkovChain struct {
+	States int
+	Trans  []float64
+}
+
+func (ch *MarkovChain) Next(state int) int {
+	r := rand.Float64()
+	var cur float64
+	st := ch.States * state
+	for i, v := range ch.Trans[st : st+ch.States] {
+		cur += v
+		if r <= cur {
+			return i
+		}
+	}
+	return ch.States - 1
+}
+
 func main() {
-	fmt.Printf("Sunny\n")
+	rand.Seed(time.Now().UnixNano())
+	weather := []string{"Sunny", "Cloudy", "Rainy"}
+	day2day := []float64{
+		/* Sunny */ 0.8, 0.2, 0,
+		/* Cloudy */ 0.4, 0.4, 0.2,
+		/* Rainy */ 0.2, 0.6, 0.2,
+	}
+	chain := &MarkovChain{
+		States: len(weather),
+		Trans:  day2day,
+	}
+	cur := 0
+	for i := 0; i < 20; i++ {
+		fmt.Printf("%s\n", weather[cur])
+		cur = chain.Next(cur)
+	}
+	fmt.Printf("\n")
 }
