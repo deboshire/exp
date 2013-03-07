2dcbb83b3f526e3678cb402408147fdca9812314
Ivan Krasin <imkrasin@gmail.com>
2013-03-07 04:30:39 +0000

    Transform image

--- webcam/js/main.js
+++ webcam/js/main.js
@@ -16,6 +16,8 @@ NodeList.prototype.each = function(callback) {
     var videoElement = document.querySelector('video');
     var c1 = document.getElementById("c1");
     var ctx1 = c1.getContext("2d");
+    var c2 = document.getElementById("c2");
+    var ctx2 = c2.getContext("2d");
 
     var screenCast = new ScreenCast(videoElement);
 
@@ -25,7 +27,22 @@ NodeList.prototype.each = function(callback) {
     for (var i = 0, numberOfButtons = effectButtons.length; i < numberOfButtons; i++) {
         effectButtons[i].addEventListener('click', function(e) {
             e.preventDefault();
-	    ctx1.drawImage(videoElement, 0, 0, videoElement.videoWidth, videoElement.videoHeight);
+	    var width = videoElement.videoWidth;
+	    var height = videoElement.videoHeight;
+	    ctx1.drawImage(videoElement, 0, 0, width, height);
+	    var frame = ctx1.getImageData(0, 0, width, height);
+	    var l = frame.data.length / 4;
+	    for (var i = 0; i < l; i++) {
+		var r = frame.data[i*4];
+		var g = frame.data[i*4+1];
+		var b = frame.data[i*4+2];
+		if (g > 100 && r > 100 && b > 43) {
+		    frame.data[i*4] = 0;
+		    frame.data[i*4+1] = 0;
+		    frame.data[i*4+2] = 0;
+		}
+	    }
+	    ctx2.putImageData(frame, 0, 0);
 
             var effect = this.getAttribute('data-effect');
 
