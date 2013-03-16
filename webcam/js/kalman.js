52675d7c876ba2d07fd7264b41a7ead4913b1acc
Ivan Krasin <imkrasin@gmail.com>
2013-03-16 16:47:36 +0000

    Split update and predict in Kalman

--- webcam/js/kalman.js
+++ webcam/js/kalman.js
@@ -17,14 +17,29 @@ Kalman = function(A, R, C, Q, mu, sigma) {
 };
 
 /*
- * Updates mu and sigma by looking at the current measurements vector z.
+ * Make prediction about mu and sigma.
+ * Return [mu_pred, sigma_pred].
+ */
+Kalman.prototype.predict = function() {
+    console.log("Kalman.predict(A=", this.A.inspect(), "C=", this.C.inspect(),
+		"mu=", this.mu.inspect());
+    var mu_pred = this.A.multiply(this.mu);
+    console.log("mu_pred: ", mu_pred.inspect());
+    var sigma_pred = this.A.multiply(this.sigma.multiply(this.A.transpose())).add(this.R);
+    console.log("sigma_pred: ", sigma_pred.inspect());
+    return [mu_pred, sigma_pred];
+}
+
+/*
+ * Update mu and sigma by looking at the current measurements vector z.
  */
 Kalman.prototype.update = function(z) {
     console.log("Kalman.update(A=", this.A.inspect(), "C=", this.C.inspect(),
 		"mu=", this.mu.inspect(), "z=", z.inspect());
-    var mu_pred = this.A.multiply(this.mu);
+    var pred = this.predict();
+    var mu_pred = pred[0];
+    var sigma_pred = pred[1];
     console.log("mu_pred: ", mu_pred.inspect());
-    var sigma_pred = this.A.multiply(this.sigma.multiply(this.A.transpose())).add(this.R);
     console.log("sigma_pred: ", sigma_pred.inspect());
     var tmp = (this.C.multiply(sigma_pred.multiply(this.C.transpose())).add(this.Q)).inverse();
     var K = sigma_pred.multiply(this.C.transpose().multiply(tmp));
