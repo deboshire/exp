2ef2e8a8c0f78bfe75ff02238a279409c26d2abf
Mike Aizatsky <mike.aizatsky@gmail.com>
2013-03-17 13:45:05 -0700

    Reworked examples. Searching for optimal lambda.

--- examples/pgm7/pgm7_test.go
+++ examples/pgm7/pgm7_test.go
@@ -4,17 +4,59 @@ import (
 	"fmt"
 	"github.com/deboshire/exp/ai"
 	"github.com/deboshire/exp/io/mat"
+	v "github.com/deboshire/exp/math/vector"
+	"github.com/deboshire/exp/optimization/gcsearch"
 	"github.com/deboshire/exp/optimization/sgrad"
+	"math/rand"
 )
 
-func benchmark(iterations int)
+func readTrainData() (trainFeatures []v.F64, trainLabels v.B) {
+	trainFeatures = mat.MustRead("Train1X.mat").Array("Train1X").RowsToVectors()
+	trainLabels = mat.MustRead("Train1Y.mat").Array("Train1Y").ToVector().F64ToB()
+	return
+}
 
-func ExamplePGM7_LogisticRegression() {
-	trainFeatures := mat.MustRead("Train1X.mat").Array("Train1X").RowsToVectors()
-	trainLabels := mat.MustRead("Train1Y.mat").Array("Train1Y").ToVector().F64ToB()
+func readBenchmarkData() (benchmarkFeatures []v.F64, benchmarkLabels v.B) {
+	benchmarkFeatures = mat.MustRead("Validation1X.mat").Array("Validation1X").RowsToVectors()
+	benchmarkLabels = mat.MustRead("Validation1Y.mat").Array("Validation1Y").ToVector().F64ToB()
+	return
+}
 
-	benchmarkFeatures := mat.MustRead("Validation1X.mat").Array("Validation1X").RowsToVectors()
-	benchmarkLabels := mat.MustRead("Validation1Y.mat").Array("Validation1Y").ToVector().F64ToB()
+func ExamplePGM7_LogisticRegression_HoldoutTesting() {
+	rand.Seed(98765)
+	trainFeatures, trainLabels := readTrainData()
+
+	for _, testingFraction := range []float64{0.5, 0.25, 0.1, 0.05} {
+		fmt.Println("---\nfraction: ", testingFraction)
+		score := ai.HoldoutTestBinaryClassifier(
+			trainFeatures,
+			trainLabels,
+			testingFraction,
+			ai.NewLogisticRegressionTrainer(0,
+				&sgrad.NumIterationsCrit{NumIterations: 10},
+				1e-8))
+		fmt.Println("Holdout testing:", score)
+	}
+
+	// Output:
+	// ---
+	// fraction:  0.5
+	// Holdout testing: 0.84
+	// ---
+	// fraction:  0.25
+	// Holdout testing: 0.92
+	// ---
+	// fraction:  0.1
+	// Holdout testing: 0.85
+	// ---
+	// fraction:  0.05
+	// Holdout testing: 1
+}
+
+func ExamplePGM7_LogisticRegression_Iterations() {
+	rand.Seed(98765)
+	trainFeatures, trainLabels := readTrainData()
+	benchmarkFeatures, benchmarkLabels := readBenchmarkData()
 
 	for _, iterations := range []int{1, 10, 100, 1000} {
 		fmt.Println("---\niterations: ", iterations)
@@ -22,32 +64,8 @@ func ExamplePGM7_LogisticRegression() {
 			trainFeatures,
 			trainLabels,
 			0,
-			&sgrad.NumIterationsCriterion{NumIterations: iterations},
+			&sgrad.NumIterationsCrit{NumIterations: iterations},
 			1e-8)
-		fmt.Println("train set: ", ai.EvaluateBinaryClassifier(classifier, trainFeatures, trainLabels))
-		fmt.Println("benchmark set: ", ai.EvaluateBinaryClassifier(classifier, benchmarkFeatures, benchmarkLabels))
-	}
-
-	for _, epsilon := range []float64{1e-1, 1e-2, 1e-3} {
-		fmt.Println("---\nepsilon: ", epsilon)
-		classifier := ai.TrainLogisticRegressionClassifier(
-			trainFeatures,
-			trainLabels,
-			0,
-			&sgrad.RelativeMeanImprovementCriterion{},
-			epsilon)
-		fmt.Println("train set: ", ai.EvaluateBinaryClassifier(classifier, trainFeatures, trainLabels))
-		fmt.Println("benchmark set: ", ai.EvaluateBinaryClassifier(classifier, benchmarkFeatures, benchmarkLabels))
-	}
-
-	for _, alpha := range []float64{0, 0.1, 0.2, 0.4, 0.8, 1} {
-		fmt.Println("---\nalpha: ", alpha)
-		classifier := ai.TrainLogisticRegressionClassifier(
-			trainFeatures,
-			trainLabels,
-			alpha,
-			&sgrad.RelativeMeanImprovementCriterion{},
-			1e-2)
 		fmt.Println("train set: ", ai.EvaluateBinaryClassifier(classifier, trainFeatures, trainLabels))
 		fmt.Println("benchmark set: ", ai.EvaluateBinaryClassifier(classifier, benchmarkFeatures, benchmarkLabels))
 	}
@@ -55,12 +73,12 @@ func ExamplePGM7_LogisticRegression() {
 	// Output:
 	// ---
 	// iterations:  1
-	// train set:  0.925
-	// benchmark set:  0.915
+	// train set:  0.955
+	// benchmark set:  0.94
 	// ---
 	// iterations:  10
 	// train set:  0.985
-	// benchmark set:  0.93
+	// benchmark set:  0.925
 	// ---
 	// iterations:  100
 	// train set:  1
@@ -68,44 +86,115 @@ func ExamplePGM7_LogisticRegression() {
 	// ---
 	// iterations:  1000
 	// train set:  1
-	// benchmark set:  0.92
+	// benchmark set:  0.93
+}
+
+func ExamplePGM7_LogisticRegression_Epsilon() {
+	rand.Seed(98765)
+	trainFeatures, trainLabels := readTrainData()
+	benchmarkFeatures, benchmarkLabels := readBenchmarkData()
+
+	for _, epsilon := range []float64{1e-1, 1e-2, 1e-3} {
+		fmt.Println("---\nepsilon: ", epsilon)
+		classifier := ai.TrainLogisticRegressionClassifier(
+			trainFeatures,
+			trainLabels,
+			0,
+			&sgrad.RelativeMeanImprovementCrit{},
+			epsilon)
+		fmt.Println("train set: ", ai.EvaluateBinaryClassifier(classifier, trainFeatures, trainLabels))
+		fmt.Println("benchmark set: ", ai.EvaluateBinaryClassifier(classifier, benchmarkFeatures, benchmarkLabels))
+	}
+
+	// Output:
 	// ---
 	// epsilon:  0.1
-	// train set:  0.985
-	// benchmark set:  0.925
+	// train set:  0.99
+	// benchmark set:  0.94
 	// ---
 	// epsilon:  0.01
-	// train set:  1
+	// train set:  0.99
 	// benchmark set:  0.925
 	// ---
 	// epsilon:  0.001
 	// train set:  1
 	// benchmark set:  0.93
+}
+
+func ExamplePGM7_LogisticRegression_Alpha() {
+	rand.Seed(98765)
+	trainFeatures, trainLabels := readTrainData()
+	benchmarkFeatures, benchmarkLabels := readBenchmarkData()
+
+	for _, alpha := range []float64{0, 0.1, 0.2, 0.4, 0.8, 1} {
+		fmt.Println("---\nalpha: ", alpha)
+		classifier := ai.TrainLogisticRegressionClassifier(
+			trainFeatures,
+			trainLabels,
+			alpha,
+			&sgrad.RelativeMeanImprovementCrit{},
+			1e-2)
+		fmt.Println("train set: ", ai.EvaluateBinaryClassifier(classifier, trainFeatures, trainLabels))
+		fmt.Println("benchmark set: ", ai.EvaluateBinaryClassifier(classifier, benchmarkFeatures, benchmarkLabels))
+	}
+
+	// Output:
 	// ---
 	// alpha:  0
 	// train set:  1
-	// benchmark set:  0.94
+	// benchmark set:  0.93
 	// ---
 	// alpha:  0.1
-	// train set:  0.945
+	// train set:  0.94
 	// benchmark set:  0.93
 	// ---
 	// alpha:  0.2
 	// train set:  0.94
-	// benchmark set:  0.905
+	// benchmark set:  0.925
 	// ---
 	// alpha:  0.4
-	// train set:  0.87
-	// benchmark set:  0.855
+	// train set:  0.925
+	// benchmark set:  0.915
 	// ---
 	// alpha:  0.8
-	// train set:  0.91
-	// benchmark set:  0.885
+	// train set:  0.875
+	// benchmark set:  0.86
 	// ---
 	// alpha:  1
-	// train set:  0.88
-	// benchmark set:  0.87
+	// train set:  0.905
+	// benchmark set:  0.905
 }
 
-func init() {
+func ExamplePGM7_LogisticRegression_OptimizeAlpha() {
+	rand.Seed(98765)
+	trainFeatures, trainLabels := readTrainData()
+	benchmarkFeatures, benchmarkLabels := readBenchmarkData()
+
+	goalFunc := func(alpha float64) float64 {
+		score := ai.HoldoutTestBinaryClassifier(
+			trainFeatures,
+			trainLabels,
+			.1,
+			ai.NewLogisticRegressionTrainer(
+				alpha,
+				&sgrad.NumIterationsCrit{NumIterations: 10},
+				1e-8))
+		return -score
+	}
+
+	alpha := gcsearh.Minimize(0, 10, goalFunc, &gcsearh.AbsoluteErrorTermCrit{}, .1)
+	fmt.Println("Optimal alpha:", alpha)
+	classifier := ai.TrainLogisticRegressionClassifier(
+		trainFeatures,
+		trainLabels,
+		alpha,
+		&sgrad.RelativeMeanImprovementCrit{},
+		1e-2)
+	fmt.Println("train set: ", ai.EvaluateBinaryClassifier(classifier, trainFeatures, trainLabels))
+	fmt.Println("benchmark set: ", ai.EvaluateBinaryClassifier(classifier, benchmarkFeatures, benchmarkLabels))
+
+	// Output:
+	// Optimal alpha: 1.4841053312063623
+	// train set:  0.895
+	// benchmark set:  0.9
 }
