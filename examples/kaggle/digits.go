1a8fa4d118e56ea2a9e66905a1146d1a01aa8d66
Mike Aizatsky <mike.aizatsky@gmail.com>
2013-03-17 13:45:26 -0700

    Begginning working on waggle digits challenge.

---
+++ examples/kaggle/digits.go
@@ -0,0 +1,69 @@
+package main
+
+import (
+	"bufio"
+	"encoding/csv"
+	"fmt"
+	"github.com/deboshire/exp/ai"
+	v "github.com/deboshire/exp/math/vector"
+	"github.com/deboshire/exp/optimization/sgrad"
+	"os"
+	"strconv"
+)
+
+func parseVector(strs []string) (res v.F64, err error) {
+	res = v.Zeroes(len(strs))
+
+	for i, str := range strs {
+		parsedFloat, err := strconv.ParseFloat(str, 64)
+		if err != nil {
+			return res, err
+		}
+		res[i] = parsedFloat
+	}
+	return
+}
+
+func main() {
+	fmt.Print("Reading training data...")
+	file, err := os.Open(os.ExpandEnv("$HOME/Dropbox/Projects/kaggle/digits/train.csv"))
+	if err != nil {
+		panic(err)
+	}
+	csvReader := csv.NewReader(bufio.NewReader(file))
+	allData, err := csvReader.ReadAll()
+	if err != nil {
+		panic(err)
+	}
+
+	allData = allData[1:] // remove header
+
+	fmt.Println("done:", len(allData), "rows")
+	labels := make([]int, len(allData))
+	pixels := make([]v.F64, len(allData))
+
+	// TODO: add bias
+
+	for i, row := range allData {
+		parsedLabel, err := strconv.ParseInt(row[0], 10, 32)
+		if err != nil {
+			panic(err)
+		}
+		labels[i] = int(parsedLabel)
+		pixels[i], err = parseVector(row[1:])
+	}
+
+	binClassifierTrainer := func(features []v.F64, labels []bool) ai.BinaryClassifier {
+		fmt.Println("Training binary classifier")
+		classifier := ai.TrainLogisticRegressionClassifier(
+			features,
+			labels,
+			0,
+			&sgrad.NumIterationsCrit{NumIterations: 10},
+			1e-8)
+		return classifier
+	}
+
+	classifier := ai.TrainMultinomialClassifierFromBinary(pixels, labels, 10, binClassifierTrainer)
+	fmt.Println(classifier)
+}
