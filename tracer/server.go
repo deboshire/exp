840148df7c474257425a35293e29cdcd7d288682
Mike Aizatsky <mike.aizatsky@gmail.com>
2013-04-30 23:02:29 -0700

    Draft version of http tracer server

---
+++ tracer/server.go
@@ -0,0 +1,150 @@
+package tracer
+
+import (
+	"flag"
+	"fmt"
+	"github.com/deboshire/exp/math/vector"
+	"go/build"
+	"html/template"
+	"io"
+	"math/rand"
+	"net/http"
+	"path"
+	"runtime"
+	"sort"
+	"sync"
+)
+
+var (
+	data       = make(map[string][]float64)
+	mu         = &sync.Mutex{}
+	tracerPort = flag.Int("tracer-port", 1234, "Port to use for tracer")
+
+	templateDir string
+	staticDir   string
+)
+
+type webTracer struct {
+	prefix string
+}
+
+func NewWebTracer() Tracer {
+	result := &webTracer{}
+	go result.start()
+	return result
+}
+
+type IndexPageData struct {
+	Names []string
+}
+
+func executeTemplate(name string, data interface{}, w io.Writer) {
+	templateName := path.Join(templateDir, name)
+
+	t := template.Must(template.ParseFiles(templateName))
+	err := t.Execute(w, data)
+	if err != nil {
+		panic(err)
+	}
+}
+
+type VarPageData struct {
+	Name   string
+	Values []float64
+}
+
+func handleVar(w http.ResponseWriter, r *http.Request) {
+	var pageData VarPageData
+	pageData.Name = r.URL.Query().Get("n")
+
+	{
+		mu.Lock()
+		defer mu.Unlock()
+
+		pageData.Values = data[pageData.Name]
+	}
+
+	executeTemplate("v.html", &pageData, w)
+}
+
+func handleIndex(w http.ResponseWriter, r *http.Request) {
+	var pageData IndexPageData
+	{
+		mu.Lock()
+		defer mu.Unlock()
+
+		// This is unbelievable that in XXI century you have to manually copy keys out.
+		names := make([]string, len(data))
+		i := 0
+		for k, _ := range data {
+			names[i] = k
+			i++
+		}
+		sort.Strings(names)
+		pageData.Names = names
+	}
+
+	executeTemplate("index.html", &pageData, w)
+}
+
+func handleRequest(w http.ResponseWriter, r *http.Request) {
+	switch r.URL.Path {
+	case "/":
+		handleIndex(w, r)
+	case "/v":
+		handleVar(w, r)
+	default:
+		http.NotFound(w, r)
+	}
+}
+
+func (t *webTracer) TraceFloat64(label string, value float64) {
+	mu.Lock()
+	defer mu.Unlock()
+
+	key := t.prefix + "." + label
+	data[key] = append(data[key], value)
+}
+
+func (t *webTracer) TraceF64(label string, value vector.F64) {
+}
+
+func (t *webTracer) TraceInt(label string, value int) {
+}
+
+func (t *webTracer) Sub(label string) Tracer {
+	if len(t.prefix) > 0 {
+		label = "." + label
+	}
+	return &webTracer{prefix: t.prefix + label}
+}
+
+func (t *webTracer) Algorithm(name string) Tracer {
+	return t.Sub(fmt.Sprintf("%s.%x", name, rand.Int()))
+}
+
+func (t *webTracer) start() {
+	http.Handle("/s/", http.StripPrefix("/s/", http.FileServer(http.Dir(staticDir))))
+	http.HandleFunc("/", handleRequest)
+	fmt.Printf("Tracer started on http://localhost:%d/\n", *tracerPort)
+	if err := http.ListenAndServe(fmt.Sprintf(":%d", *tracerPort), nil); err != nil {
+		panic(err)
+	}
+}
+
+func initDirs() {
+	ctx := build.Default
+	// todo(mike): can you reference current package somehow?
+	pkg, err := ctx.Import("github.com/deboshire/exp/tracer", "", build.FindOnly)
+	if err != nil {
+		panic(err)
+	}
+	dir := pkg.Dir
+	templateDir = path.Join(dir, "t")
+	staticDir = path.Join(dir, "s")
+}
+
+func init() {
+	runtime.GOMAXPROCS(runtime.NumCPU())
+	initDirs()
+}
--- tracer/tracer.go
+++ tracer/tracer.go
@@ -15,48 +15,90 @@ type Tracer interface {
 	TraceFloat64(label string, value float64)
 	TraceF64(label string, value vector.F64)
 	TraceInt(label string, value int)
+
+	Sub(label string) Tracer
+	Algorithm(label string) Tracer
 }
+
+var (
+	defaultTracer Tracer
+)
 
 type tracerImpl struct {
 	p string
 	w io.Writer
 }
 
-func (t tracerImpl) TraceFloat64(label string, value float64) {
-	fmt.Fprintf(t.w, "%s%s : %v\n", t.p, label, value)
+func (t *tracerImpl) TraceFloat64(label string, value float64) {
+	fmt.Fprintf(t.w, "%s.%s : %v\n", t.p, label, value)
 }
 
-func (t tracerImpl) TraceInt(label string, value int) {
-	fmt.Fprintf(t.w, "%s%s : %v\n", t.p, label, value)
+func (t *tracerImpl) TraceInt(label string, value int) {
+	fmt.Fprintf(t.w, "%s.%s : %v\n", t.p, label, value)
 }
 
-func (t tracerImpl) TraceF64(label string, value vector.F64) {
-	fmt.Fprintf(t.w, "%s%s : %v\n", t.p, label, value)
+func (t *tracerImpl) TraceF64(label string, value vector.F64) {
+	fmt.Fprintf(t.w, "%s.%s : %v\n", t.p, label, value)
+}
+
+func (t *tracerImpl) Sub(label string) Tracer {
+	if len(t.p) > 0 {
+		label = "." + label
+	}
+	return &tracerImpl{p: t.p + label, w: t.w}
+}
+
+func (t *tracerImpl) Algorithm(name string) Tracer {
+	return t.Sub(name)
 }
 
 type nullTracer struct{}
 
-func (t nullTracer) TraceFloat64(label string, value float64) {
+func (t *nullTracer) TraceFloat64(label string, value float64) {
 }
 
-func (t nullTracer) TraceInt(label string, value int) {
+func (t *nullTracer) TraceInt(label string, value int) {
 }
 
-func (t nullTracer) TraceF64(label string, value vector.F64) {
+func (t *nullTracer) TraceF64(label string, value vector.F64) {
+}
+
+func (t *nullTracer) Sub(label string) Tracer {
+	return t
+}
+
+func (t *nullTracer) Algorithm(name string) Tracer {
+	return t
 }
 
 func NewNullTracer() Tracer {
-	return nullTracer{}
+	return &nullTracer{}
 }
 
 func NewTracer(prefix string, writer io.Writer) Tracer {
-	return tracerImpl{p: prefix, w: writer}
+	return &tracerImpl{p: prefix, w: writer}
 }
 
 func NewStderrTracer(prefix string) Tracer {
-	return tracerImpl{p: prefix, w: os.Stderr}
+	return &tracerImpl{p: prefix, w: os.Stderr}
+}
+
+func NewFileTracer(fileName string) Tracer {
+	f, err := os.Create(fileName)
+	if err != nil {
+		panic(err)
+	}
+	return &tracerImpl{p: "", w: f}
 }
 
 func DefaultTracer() Tracer {
-	return NewNullTracer()
+	return defaultTracer
+}
+
+func SetDefaultTracer(t Tracer) {
+	defaultTracer = t
+}
+
+func init() {
+	defaultTracer = NewNullTracer()
 }
