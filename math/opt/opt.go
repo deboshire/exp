cea75d29472ffd81558a89009e49cd6d3583b1a9
Mike Aizatsky <mike.aizatsky@gmail.com>
2013-06-01 16:09:55 -0700

    Converge() function as a base for optimizations.

---
+++ math/opt/opt.go
@@ -0,0 +1,127 @@
+package opt
+
+import (
+	"github.com/deboshire/exp/math/stat"
+	"github.com/deboshire/exp/math/vector"
+	"math"
+)
+
+type TermCrit interface {
+	ShouldTerminate(s *State) float64
+}
+
+type State struct {
+	Epoch     int
+	Iter      int
+	TotalIter int
+	// previous X value
+	X vector.F64
+	// new X value
+	X1 vector.F64
+
+	// Number of iterations performed in previous epochs
+	EpochIter []int
+}
+
+// Converge applies the function until it converges.
+// f takes the old vectox X and stores result into x1.
+func Converge(f func(x vector.F64, x1 *vector.F64), x0 vector.F64, termCrit TermCrit, eps float64) vector.F64 {
+	return ConvergeWithState(f, NewState(x0), termCrit, eps)
+}
+
+func NewState(x0 vector.F64) *State {
+	return &State{X: x0.Copy(), X1: x0.Copy()}
+}
+
+func ConvergeWithState(f func(x vector.F64, x1 *vector.F64), state *State, termCrit TermCrit, eps float64) vector.F64 {
+	state.Epoch++
+	state.Iter = 0
+
+	for {
+		f(state.X, &state.X1)
+
+		// fmt.Println("x:", state.X, "x1: ", state.X1)
+
+		if termCrit.ShouldTerminate(state) <= eps {
+			state.EpochIter = append(state.EpochIter, state.Iter+1)
+			return state.X1
+		}
+
+		temp := state.X
+		state.X = state.X1
+		state.X1 = temp
+		state.Iter++
+		state.TotalIter++
+	}
+}
+
+// Termination criterion that terminates after given number of iterations.
+type NumIterationsCrit struct {
+	NumIterations int
+}
+
+func (c *NumIterationsCrit) ShouldTerminate(s *State) float64 {
+	if s.Iter >= c.NumIterations-1 {
+		return 0
+	}
+	return math.MaxFloat64
+}
+
+// Tracks mean of relative coordinate change vector length.
+type MaxRelativeChangeCrit struct {
+	Iters  int
+	window *stat.Window
+}
+
+func (c *MaxRelativeChangeCrit) ShouldTerminate(s *State) float64 {
+	if c.window == nil {
+		if c.Iters < 1 {
+			c.Iters = 10
+		}
+
+		c.window = &stat.Window{N: c.Iters}
+	}
+
+	dx := s.X.Copy()
+	dx.Sub(s.X1)
+	dxl := dx.Length()
+	sxl := s.X.Length()
+
+	val := dxl / sxl
+
+	c.window.Add(val)
+
+	if !c.window.Full() {
+		return math.MaxFloat64
+	}
+
+	// fmt.Println("dxl:", dxl, "sxl:", sxl, "val:", val)
+	return c.window.Max()
+}
+
+// Tracks mean of relative coordinate change vector length.
+type MaxAbsChangeCrit struct {
+	Iters  int
+	window *stat.Window
+}
+
+func (c *MaxAbsChangeCrit) ShouldTerminate(s *State) float64 {
+	if c.window == nil {
+		if c.Iters < 1 {
+			c.Iters = 10
+		}
+
+		c.window = &stat.Window{N: c.Iters}
+	}
+
+	dx := s.X.Copy()
+	dx.Sub(s.X1)
+	dxl := dx.Length()
+	c.window.Add(dxl)
+
+	if !c.window.Full() {
+		return math.MaxFloat64
+	}
+
+	return c.window.Max()
+}
